8095a18322196a25682f88aaa4710cb60f724574
jachymsulovsky <sulovskyjachym@gmail.com>
2026-06-06 15:56:04 +0200

    Externalize gtag init and allow unpkg in CSP

---
+++ public/js/gtag-init.js
@@ -0,0 +1,11 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+
+// Default to denied until user gives consent via cookie banner
+gtag('consent', 'default', {
+  'analytics_storage': 'denied',
+  'ad_storage': 'denied'
+});
+
+gtag('js', new Date());
+gtag('config', 'G-V8KTRE2T9Y');
